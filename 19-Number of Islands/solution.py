210c7b89bf7a0078d5eec3aa170758d6e2f62311
Yahya Murad <yahyamurad197@gmail.com>
2024-04-19 10:15:36 +0300

    Added "Number of Islands" python solution

--- 19-Number of Islands/solution.py
+++ 19-Number of Islands/solution.py
@@ -1,3 +1,37 @@
+from collections import deque
 class Solution:
     def numIslands(self, grid):
-        pass
+        if not grid:
+            return 0
+
+        rows, cols = len(grid), len(grid[0])
+        vis = set()
+        res = 0
+
+        def bfs(r, c):
+            queue = deque()
+            vis.add((r, c))
+            queue.append((r, c))
+
+            while queue:
+                row, col = queue.popleft()
+                directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+
+                for x, y in directions:
+                    print("CHECKING", (x, y))
+                    r, c = row + x, col + y
+                    if r in range(rows) and \
+                       c in range(cols) and \
+                       grid[r][c] == "1" and \
+                       (r, c) not in vis:
+                        queue.append((r, c))
+                        vis.add((r, c))
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in vis:
+                    bfs(r, c)
+                    res += 1
+                    print()
+
+        return res
