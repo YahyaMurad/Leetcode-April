22518e542e55d02356e8a53c7689c37d408059ac
Yahya Murad <yahyamurad197@gmail.com>
2024-04-28 17:12:22 +0800

    Changed to DP solution

--- 27-Freedom Trail/solution.py
+++ 27-Freedom Trail/solution.py
@@ -1,24 +1,18 @@
 class Solution:
     def findRotateSteps(self, ring, key):
-        memo = {}
-        def helper(r, k):
-            if k == len(key):
-                return 0
-            
-            if (r, k) in memo:
-                return memo[(r, k)]
-            
-            res = float("INF")
+        dp = [0] * len(ring)
 
-            for i, c in enumerate(ring):
-                if c == key[k]:
-                    minDistance = min(abs(r - i), len(ring) - abs(r - i))
-                    res = min(res, minDistance + 1 + helper(i, k + 1))
-            
-            memo[(r, k)] = res
-            return res
-        
-        return helper(0, 0)
+        for k in reversed(range(len(key))):
+            next_dp = [float("INF")] * len(ring)
+            for r in range(len(ring)):
+                for i, c in enumerate(ring):
+                    if c == key[k]:
+                        minDistance = min(abs(r - i), len(ring) - abs(r - i))
+                        next_dp[r] = min(next_dp[r], minDistance + 1 + dp[i])
+                
+            dp = next_dp
+
+        return dp[0]
 
 sol = Solution()
 print(sol.findRotateSteps("godding", "gd"))
